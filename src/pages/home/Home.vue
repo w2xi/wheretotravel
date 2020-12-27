fb6f720ef67d69c82eebd8b3bc0ae726b1b1b804
w2xi <43wangxi@gmail.com>
2020-12-27 22:50:44 +0800

    project init

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,8 +1,5 @@
 <template>
-	<div>
-		<div class="home">Home</div>
-		<router-link to="/list" class="home">List</router-link>
-	</div>
+	<div>hello world</div>
 </template>
 
 <script>
@@ -12,10 +9,5 @@ export default {
 </script>
 
 <style>
-.home {
-	padding: 20px 40px;
-	background-color: #f1a166;
-	border: 1px solid #f0cc12;
-	font-size: 40px;
-}
+
 </style>
