6b8f5aebcd7623ba038bbbc4b01543080a399d14
w2xi <43wangxi@gmail.com>
2021-06-09 21:34:46 +0800

    add vuex

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import fastClick from 'fastclick'
+import store from './store'
 import './assets/styles/reset.css'
 import './assets/styles/iconfont.css'
 
 Vue.config.productionTip = false
-fastClick.attach(document.body)
 
 new Vue({
-  render: h => h(App),
   router,
+  store,
+  render: h => h(App),
 }).$mount('#app')
